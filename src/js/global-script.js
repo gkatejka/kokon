845ebccafff5c116913e7ce8201a4fc66d9ec428
Ekaterina Golovkina <kate.golovkina@gmail.com>
2017-06-28 02:03:28 +0300

    Added new pages

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -2,6 +2,24 @@
 
 $( document ).ready(function() {
 
+  $('.about__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    variableWidth: true
+  });
 
-
+  $('.about__slider').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Загрузка изображения #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    }
+  });
 });
