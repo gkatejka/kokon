167f9851b3e3b33e973f496e654ba62abd6fe597
Ekaterina Golovkina <kate.golovkia@gmail.com>
2017-07-22 23:52:45 +0300

    added form in modal, added interactive map

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,11 +1,24 @@
-// Свой код с jQuery
+$(document).ready(function() {
 
-$( document ).ready(function() {
+  var $toggleMenu = $('.js-toggle-btn');
+  var $navMune = $('.menu-nav');
+  var $modal = $('.modal');
+  var $body = $('body');
+  var $btnModalOpen = $('.js-btn-modal-open');
+  var $btnModalClose =$('.js-btn-modal-close')
+  var $dateInput = $('.js-form-date');
 
-  $('.js-toggle-btn').click(function() {
-    $('.menu-nav').toggleClass('menu-nav--open');
+  // ----
+  // Меню
+  // ----
+  $toggleMenu.click(function() {
+    $navMune.toggleClass('menu-nav--open');
   });
 
+
+  // --------
+  // Слайдеры
+  // --------
   $('.about__slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -23,7 +36,64 @@ $( document ).ready(function() {
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0,1],
     }
   });
+
+
+  // ------
+  // Селект
+  // ------
+  $('.select').selectize({
+    allowEmptyOption: true,
+  });
+
+
+  // ---------
+  // Календарь
+  // ---------
+  $dateInput.click(function() {
+    $(this).attr('type', 'date').datepicker('show');
+  });
+
+  $dateInput.datepicker({
+    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    firstDay: 1,
+    dateFormat: 'dd.mm.yy',
+  });
+
+  $dateInput.focusout(function() {
+    var newDate = $('.js-form-date').val();
+
+    if (newDate.length > 0) {
+      $(this).attr('type', 'text').val(dateFinal);
+    } else {
+      $(this).attr('type', 'text');
+    }
+  });
+
+
+  // --------------
+  // Модальное окно
+  // --------------
+  var closeModal = function() {
+    $modal.removeClass('modal-open');
+    $body.removeClass('overlay');
+  }
+
+  $btnModalOpen.click( function(e) {
+    e.preventDefault();
+    $modal.addClass('modal-open');
+    $body.addClass('overlay');
+  });
+
+  $body.mouseup(function (e) {
+    if ($body.hasClass('overlay') && $modal.has(e.target).length === 0){
+      closeModal();
+    }
+  });
+
+  $btnModalClose.click(closeModal);
+
 });
